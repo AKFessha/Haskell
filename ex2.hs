0a52195f7c6d38505e73054d06e81ce66f40f70a
Awet <awetkifle94@gmail.com>
2020-04-13 23:49:37 +0100

    exercise 2

---
+++ ex2.hs
@@ -0,0 +1,33 @@
+import Data.Char
+
+
+
+--Ex 1
+myrepeat :: Char -> [Char]
+myrepeat = undefined
+
+
+
+-- Exercise 2 (a)
+
+myisDigit::Char -> Bool
+myisDigit = undefined
+
+-- Exercise 2 (b)
+
+mytoLower::Char -> Char
+mytoLower = undefined
+
+--Ex 3
+circlesurface :: Float -> Float
+circlesurface = undefined
+
+-- Ex 4
+cycleinc::[Integer] -> [Integer]
+cycleinc = undefined
+
+
+-- Ex 5
+
+compLetters :: Char -> Char -> Char
+compLetters = undefined
