db9f004054e2e592b008dc31013c9ed7cd88df55
Awet <awetkifle94@gmail.com>
2020-04-17 23:10:26 +0100

    exercise 3 added

---
+++ ex3.hs
@@ -0,0 +1,32 @@
+--Ex 1(a)
+checkprod::(Int,Int) -> Int ->Bool
+checkprod = undefined
+
+
+--Ex 1(b) 
+smallerprod::Int -> [(Int,Int)]
+smallerprod = undefined
+
+-- Ex 2(a)
+prime :: Int -> Bool
+prime = undefined
+
+-- Ex 2(b)
+primes::[Int]
+primes = undefined
+
+-- Ex 3
+myprefix::String->String->Bool
+myprefix = undefined
+
+-- Ex 4
+removeRep :: String -> String
+removeRep = undefined
+
+
+-- Ex 5
+vecSqDist :: [Int] -> [Int] -> Int
+vecSqDist = undefined
+
+
+
