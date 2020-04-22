9c8bc4dde52ef95a332e87a0d3ab92044188b364
Awet <awetkifle94@gmail.com>
2020-04-22 21:20:17 +0100

    add exercise 4

---
+++ ex4.hs
@@ -0,0 +1,64 @@
+--Exercise 1
+
+countocc :: Char -> String -> Int
+countocc =undefined 
+
+
+--Exercise 2
+
+data Nat = Zero | Nxt Nat
+   deriving Show
+
+--Exercise 2(a)
+absNat :: Int -> Nat
+absNat = undefined
+
+
+--Exercise 2(b)
+
+mymult::Nat->Nat->Nat
+mymult = undefined
+
+
+myadd:: Nat -> Nat -> Nat
+myadd (Zero) x = x
+myadd (Nxt x) y = Nxt (myadd x y)
+
+
+
+--Exercise 3
+--(a)
+insert :: Ord b => (a -> b) -> a -> [a] -> [a]
+insert = undefined
+
+
+
+--(b) 
+inssort :: Ord b => (a -> b) -> [a] -> [a]
+inssort = undefined
+
+--(c) Order a list of strings according to the occurences of the given character
+sortwords :: Char -> [String] -> [String]
+sortwords = undefined
+
+
+
+type Position = (Int,Int)
+data Step = L | R | U | D
+
+--Ex 4(a)
+
+move :: Step -> Position -> Position
+move = undefined
+
+-- move L (3,4) == (2,4)
+-- move U (0,0) == (0,1)
+
+--Ex 4(b)
+
+type Route = [Step]
+
+positions :: Route -> Position-> [Position]
+positions = undefined
+
+--positions [L,R,L,L,U] (0,0) == [(0,0),(-1,0),(0,0),(-1,0),(-2,0),(-2,1)]
