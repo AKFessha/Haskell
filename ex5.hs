90e956978e616eb26a30f2defbde89a76d855273
Awet <awetkifle94@gmail.com>
2020-04-27 19:33:05 +0100

    approxsqrtTwo completed

--- ex5.hs
+++ ex5.hs
@@ -37,9 +37,9 @@ sqrtHelper n [x] = x
 sqrtHelper n (x:y:xs)
              |(x- sqrt n)^2 <= (y- sqrt n)^2 = sqrtHelper n (x:xs)
              | otherwise = sqrtHelper n (y:xs)
-             
+
 approxsqrtTwo :: Int -> Float
-approxsqrtTwo n = randomRs (0,n) (mkStdGen 42)
+approxsqrtTwo n = sqrtHelper 2 (take n genFloatTwo)
 
 -- Exercise 3(b)
 genFloat::Float -> [Float]
